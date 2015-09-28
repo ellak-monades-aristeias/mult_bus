ca0725cae5329e5ac0478e3d99b6b3b643e53462
Angel Pappas <p.v.aggelos@gmail.com>
2015-09-29 00:24:52 +0300

    Update init_songs.sh

--- src/bash_scripts/init_songs.sh
+++ src/bash_scripts/init_songs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+#Replace these if needed.
 BASE_DIR="/home/pi/media_files/music/"
 MYSQL_PASS="raspberry"
 
-#Diagrafoyme ta palia tragoydia.
+#Delete the previous songs and votes from the database.
 /usr/bin/mysql -h "localhost" -u "root" -p${MYSQL_PASS} multsyst --skip-column-names -e "TRUNCATE TABLE psifoi"
 /usr/bin/mysql -h "localhost" -u "root" -p${MYSQL_PASS} multsyst --skip-column-names -e "TRUNCATE TABLE tragoudia"
 
-#Bazoyme ta kainoyria.
+#Read the new songs from the directory in the disk.
 cd "$BASE_DIR"
+#Get only the files that end with .pm3 and keep only the part before the .mp3
 tragoydia=$(/bin/ls *.mp3 2> /dev/null | /bin/sed "s/.mp3$//g" )
 ls_status=$?
 if [ $ls_status -eq 0 ]
@@ -17,12 +19,15 @@ then
     printf "%s\n" "$tragoydia" | while IFS= read -r line
     do
         #line = $(/bin/echo $line | /bin/sed "s/\'/\\\'/g")
+        #Create the query that inserts the name of the song in the database.
         query="INSERT INTO tragoudia (id, name) VALUES ($id, '$line');"
         #echo $query
+        #Actually execute the querry.
         /usr/bin/mysql -h "localhost" -u "root" -p${MYSQL_PASS} multsyst --skip-column-names -e "$query"
          id=$[$id +1]
     done
 else
+    #If the directory is empty, do nothing.
     echo "Directory $BASE_DIR does not exist or empty."
     exit 1
 fi

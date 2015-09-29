1ff6c0a0d88c14a891a58a0ef927448e1fc3efa2
Angel Pappas <p.v.aggelos@gmail.com>
2015-09-29 23:04:26 +0300

    Update init_theseis.sh

--- src/bash_scripts/init_theseis.sh
+++ src/bash_scripts/init_theseis.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
+#Replace these if needed.
 ip="10.0.0.254"
 IMAGE_DIR=images
 IMAGE_HTML=images.html
 MYSQL_PASS="raspberry"
 
-#Diagrafoyme tis plies eikones.
+#Delete the old files and images from the disk.
 /bin/rm -rf $IMAGE_DIR
 /bin/mkdir -p $IMAGE_DIR
 
-#Diagrafoyme tis palies theseis.
+#Delete the old pasword from the database.
 mysql -h "localhost" -p${MYSQL_PASS} -u "root" multsyst --skip-column-names -e "TRUNCATE TABLE theseis"
 
+#Create an html file with the seats and the corresponding passwords.
 echo "<!DOCTYPE html>" > $IMAGE_HTML
 echo "<html>" >> $IMAGE_HTML
 echo "<body style=\"font-size: 4em;\">" >> $IMAGE_HTML
 
-#Bazoyme tis theseis
+#Insert the seats
 for i in $(seq 1 51)
 do
-    #Tyxaios tetrapsifios aritjmos
+    #Create a randon 4 digit password.
     pass=$[ ( $RANDOM % 9000) + 1000 ]
+    #Create an image iwth the qr code.
     /usr/bin/qrencode -s 7 -o "$IMAGE_DIR/$i.png" "http://$ip/index.php?thesh=$i&pass=$pass"
+    #Insert the code to the dataabse.
     query="INSERT INTO theseis (id,pass) VALUES ('$i', '$pass');"
     mysql -h "localhost" -p${MYSQL_PASS} -u "root" multsyst --skip-column-names -e "$query"
 
+    #Also, append to the html file the seath, the password and the image with the qr code.
     echo "<div style=\"display: inline-block;\">" >> $IMAGE_HTML
     echo "<img src=\"$IMAGE_DIR/$i.png\" title=\"Home\"/>" >> $IMAGE_HTML
     echo "<br/>" >> $IMAGE_HTML

0dcaf77aecca2546f4b4c5e948a18e3b92ad31af
Angel Pappas <p.v.aggelos@gmail.com>
2015-09-29 23:06:33 +0300

    Update init_video.sh

--- src/bash_scripts/init_video.sh
+++ src/bash_scripts/init_video.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+#Replace these if needed.
 BASE_DIR="/home/pi/media_files/video/"
 MYSQL_PASS="raspberry"
 
-#Diagrafoyme ta palia video.
+#Delete the previous videos and votes from the database.
 /usr/bin/mysql -p${MYSQL_PASS} -h "localhost" -u "root" multsyst --skip-column-names -e "TRUNCATE TABLE psifoi_video"
 /usr/bin/mysql -p${MYSQL_PASS} -h "localhost" -u "root" multsyst --skip-column-names -e "TRUNCATE TABLE video"
 
-#Bazoyme ta kainoyria.
+#Read the new videos from the directory in the disk.
 cd "$BASE_DIR"
+#Get only the files that end with .avi and keep only the part before the .avi
 video=$(/bin/ls *.avi 2> /dev/null | /bin/sed "s/.avi$//g" )
 ls_status=$?
 if [ $ls_status -eq 0 ]
@@ -17,12 +19,15 @@ then
     printf "%s\n" "$video" | while IFS= read -r line
     do
         #line = $(/bin/echo $line | /bin/sed "s/\'/\\\'/g")
+        #Create the query that inserts the name of the song in the database.
         query="INSERT INTO video (id, name) VALUES ($id, '$line');"
         #echo $query
+        #Actually execute the querry.
         /usr/bin/mysql -p${MYSQL_PASS} -h "localhost" -u "root" multsyst --skip-column-names -e "$query"
          id=$[$id +1]
     done
 else
+    #If the directory is empty, do nothing.
     echo "Directory $BASE_DIR does not exist or empty."
     exit 1
 fi
